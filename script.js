2149d41322e23f071545fc6497f3403e03c5b66a
DeeprajSaha <deeprajsaha401@gmail.com>
2024-12-13 20:09:06 +0530

    update

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+
+// menu option
 const hamburger = document.querySelector('.hamburger');
 const navLists = document.querySelector('.nav-lists');
 
 hamburger.addEventListener('click',()=>{
     navLists.classList.toggle('active')
 });
+
+// about section
+
+let tablinks = document.getElementsByClassName("tap-links");
+let tabcontents = document.getElementsByClassName("tap-contents");
+
+
+const opentab = (tabname) => {
+    for (let tablink of tablinks) {
+        tablink.classList.remove("active-links");
+    }
+    for (let tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-links")
+    document.getElementById(tabname).classList.add("active-tab");
+};
